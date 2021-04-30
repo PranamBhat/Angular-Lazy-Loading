f6480df4f6a4088df385ef7308b06190894255cd
Pranam Bhat <pranam707@gmail.com>
2021-04-30 09:22:09 +0530

    Initial commit for Angular Lazy Loading

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module')
+      .then(mod => mod.AboutModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./contact/contact.module')
+      .then(mod => mod.ContactModule)
+  },
+  { path: '**', component: HomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
